d821f0bafa1dc02a316f75f13c66afe3d6850786
evadogstar <evastar@protonmail.com>
2016-07-03 01:56:40 +0300

    Update qvm-screenshot-tool.sh

--- qvm-screenshot-tool.sh
+++ qvm-screenshot-tool.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-# https://github.com/evadogstar/qvm-screenshot-tool/
+
+# Take screenshot in Qubes Dom0, auto copy to AppVM, upload to imgurl service
+# Dependencies: scrot at dom0 (sudo qubes-dom0-update scrot) 
+# zenity at dom0 and at AppVM (already exists by default at fedora and dom0)
+#
+
+# Plans: 1) delayed screenshots 
+# 2) select multi regions and make one image from all of them to upload
+# 3) upload any image from dom0 after select it
+# 4) editor of screenshots before upload
 
 version="0.1beta"
 DOM0_SHOTS_DIR=$HOME/Pictures
@@ -20,7 +29,7 @@ fi
 
 # check curl is available
 type curl >/dev/null 2>/dev/null || {
-   echo "[ERROR] Couln't find curl, which is required." >&2
+   echo "[ERROR] Couln't find curl, which is required at AppVM." >&2
    exit 17
 }
 
@@ -29,9 +38,8 @@ logfile="$2"
 
    # check file exists
    if [ ! -f "$file" ]; then
-      echo "[ERROR] file '$file' doesn't exist, skipping" >&2
-      errors=true
-      continue
+      echo "[ERROR] file '$file' doesn't exist at AppVM" >&2
+      exit 18
    fi
 
    response="$(curl --compressed --connect-timeout "5" -m "150" --retry "1" -fsSL --stderr - -H "Authorization: Client-ID ${imgur_anon_id}" -F "image=@$file" https://api.imgur.com/3/image)"
@@ -44,7 +52,7 @@ logfile="$2"
        imgurl="https://i.imgur.com/${img_id}.${img_ext}"
        imgdeleteurl="https://imgur.com/delete/${del_id}"
 
-      echo -e "Image url:\n$imgurl\n\nDelete image url: \n$imgdeleteurl\n\n \nQubes Screenshot Tool by EvaDogStar 2016" > $logfile
+      echo -e "Image url:\n$imgurl\n\nDelete image url: \n$imgdeleteurl\n\n \nQubes Screenshot Tool - EvaDogStar 2016" > $logfile
 
       echo "[success] imgurl: $imgurl" >&2   
       echo "[success] delete url: $imgdeleteurl" >&2
@@ -55,17 +63,54 @@ logfile="$2"
        echo "[ERROR] $err_msg"
        echo "[RESPONSE] $response" 
 
-      echo -e "Error: \n\n$err_msg \n\nImgurl Server response: \n\n$response\n\n\n \nQubes Screenshot Tool - EvaDogStar 2016" > $logfile       
+     echo -e "Error: \n\n$err_msg \n\nImgurl Server response: \n\n$response\n\n\n \nQubes Screenshot Tool - EvaDogStar 2016" > $logfile       
        #echo ${err_msg}
    fi
 
 (which xclip &>/dev/null && echo -n "$imgurl" | xclip -selection clipboard ) || echo "[NOTE] no xclip at AppVM"
       
 
-
 EOFFILE
 )
+#kdialog --msgbox "dsadasd"
+#exit 1
+#kdialog --inputbox "Url:" "fasffasf asfsaf af asf saf sdag sdg s"  "dasdas" "fgdsgsad"
+#exit 1
 
+# vai_ksnapshootxxx()
+# {
+#   # setGrabMode notes: 0=full-screen, 1=window, 2=region
+#   #kstart -iconify ksnapshot && sleep 0.2
+#   ksnapshot &
+#   sleep 1
+#   while [ "$PID" == "" ]; do PID="$(pgrep ksnapshot)"; done
+#   program="org.kde.ksnapshot-${PID}"
+# ####  qdbus $program /KSnapshot setGrabMode "$1"
+# #####qdbus $program /KSnapshot setTime 0
+# ####kdialog --radiolist "Continue upload process? Confirm only if you are ready" continue continue continue --default continue --title "$program" --nograb --noxim  
+
+# zenity --question --text "Move this mindow away and make screenshot. When you will be ready to upload image click on OK button."
+
+#   [[ $1 -eq 2 ]] && qdbus $program /KSnapshot slotGrab
+#   [[ $1 -eq 4 ]] && qdbus $program /KSnapshot slotGrab  
+#   [[ $1 -eq 2 ]] && sleep 4
+#   #sleep 5
+#   #kdialog --msgbox "click yes when you done ?"
+
+#   # confirm dialog for region selection tool
+# ###  [[ $1 -eq 2 ]] && kdialog --radiolist "Continue upload process? Confirm only if you are ready" continue continue continue --default continue --title "$program" --nograb --noxim
+  
+#   #qdbus $program /KSnapshot setURL "$2"
+#   #qdbus $program /KSnapshot slotSave
+# PID="$(pgrep -n ksnapshot)"
+#    program="org.kde.ksnapshot-${PID}"
+#   qdbus $program /KSnapshot slotGrab  
+#   sleep 2
+
+#   qdbus $program /KSnapshot save $2
+#   echo "$2"
+#   qdbus $program /KSnapshot exit
+# }
 checkscrot()
 {  
    (which scrot &>/dev/null ) || { 
@@ -78,26 +123,39 @@ checkscrot()
 
 start_ksnapshoot()
 {
-  # while [ "$PID" == "" ]; do PID="$(pgrep -n ksnapshot)"; done
   PID="$(pgrep ksnapshot)"
   if [ "$PID" == "" ]; then
    ksnapshot &
    sleep 1
   fi
   # setGrabMode notes: 0=full-screen, 1=window, 2=region
+  #kstart ksnapshot
+  #kdialog --radiolist "Now you can user Spapshot tool to make screenshots. When and only when you will be ready with screenshot (check preview area) click OKEY. Confirm only if you are ready!" READY READY READY --default continue --title "$program" --nograb --noxim  
   zenity --question --text "Move this mindow away and make screenshot. When you will be ready to upload image click on OK button."
   
+  # while [ "$PID" == "" ]; do PID="$(pgrep -n ksnapshot)"; done
+
   # ksnap pid changed after using region selection tool
   PID="$(pgrep ksnapshot)"
   program="org.kde.ksnapshot-${PID}"
   qdbus $program /KSnapshot save $2
-  echo "ksnap saved at dome0: $2"
+  echo "ksnap save: $2"
   qdbus $program /KSnapshot exit
 }
 
-program="`basename $0`"
-mkdir -p $DOM0_SHOTS_DIR ||exit 1
 
+ (which zenity &>/dev/null ) || { 
+    scrotnomes="[FATAL] no \"zenity\" tool at dom0 installeted use: \n\nsudo qubed-dom0-update zenity command to add it first"
+    echo "$scrotnomes" 
+    exit 1 
+ }
+
+
+program="`basename $0`"
+shotslist=""
+
+mkdir -p $DOM0_SHOTS_DIR ||exit 1
+while true; do
    d=`date +"%Y-%m-%d-%H%M%S"`
    shotname=$d.png
 
@@ -111,22 +169,22 @@ mkdir -p $DOM0_SHOTS_DIR ||exit 1
 
   if [ X"$ans" == X"Ksnapshot" ]; then
    echo "[+] starting ksnapshot..."
-   start_ksnapshoot 4 $DOM0_SHOTS_DIR/$shotname
+   start_ksnapshoot 4 $DOM0_SHOTS_DIR/$shotname || break
   elif [ X"$ans" == X"Region or Window" ]; then
      checkscrot || break
      echo "[+] capturing window, click on it to select"
-     scrot -s -b $DOM0_SHOTS_DIR/$shotname
+     scrot -s -b $DOM0_SHOTS_DIR/$shotname || break
   elif [ X"$ans" == X"Fullscreen" ]; then
      checkscrot || break
      echo "[+] capturing fullscreen desktop"      
-     scrot -b $DOM0_SHOTS_DIR/$shotname
+     scrot -b $DOM0_SHOTS_DIR/$shotname || break
   else
      echo "You must select some mode to continue" && exit 1
   fi
 
   if [ -f "$DOM0_SHOTS_DIR/$shotname" ]
   then
-      echo "[+] Success at dom0. Screenshot saved at $DOM0_SHOTS_DIR/$shotname"
+      echo "[+] Success at dom0. Screenshot saved at $DOM0_SHOTS_DIR/$shotname" || break
   else
    echo "[ERROR] Something goes wrong and screenshot is not saved at dom0."
    $(zenity --info --modal --text "Something goes wrong and screenshot is NOT saved at dom0") 
@@ -135,7 +193,15 @@ mkdir -p $DOM0_SHOTS_DIR ||exit 1
 
 
    [[ $mode_quick -eq 1 ]] && exit 1
+   shotslist="$shotname"
+   #shotslist="${shotslist}${shotname}:"
+   break
 
+   # [[ $mode_multi -eq 1 ]] && kdialog --yesno "Other shot ?" || break
+done
+
+
+  
  ans=$(zenity --list --modal --width=200 --height=290 --text "Screenshot saved at dom0 \nWhat do you want to do next?\nSelect or multiselect some options:" --checklist --column "Pick" --column "Options" \
    FALSE Exit \
    FALSE "Upload to AppVM only" \
@@ -154,12 +220,15 @@ mode_not_delete_screen_at_dom=0
 
 IFSDEFAULT=$IFS
 IFS='|'; for val in $ans; 
-do case $val in
+do 
+#echo "variable: $val and $1"
+case $val in
   'Exit') echo "[+] Good Bye"; exit 1 ;;
   'Upload to AppVM only') mode_onlyupload=1;  ;;
   'Upload to Imgurl') mode_imgurl=1;  ;;
   'Start Nautilus at AppVM') mode_nautilus=1;  ;;
   'Keep screenshot at dom0') mode_not_delete_screen_at_dom=1;  ;;
+ # -r) mode_region=1;  ;;
   *) echo "Never Good Bye!"; exit 1 ;;
 esac done
 
@@ -181,7 +250,7 @@ if [ X"$appvm" != X"" ]; then
       sleep 1
    fi
 
-   shot=$shotname
+   shot=$shotslist
 
    echo "[-] coping screenshot to $APPVM_SHOTS_DIR/$shot"
    cat $DOM0_SHOTS_DIR/$shot \
@@ -194,7 +263,8 @@ if [ X"$appvm" != X"" ]; then
    [[ $mode_imgurl -eq 0 ]] && exit 1
 
    echo "[-] copying imgurl uploader to AppVM $appvm"
-
+#      echo $UPLOADHELPER \
+#          | qvm-run --pass-io $appvm "echo $UPLOADHELPER > $APPVM_SHOTS_DIR/autouplodertemp.sh"
    uploadername='evauploadermgur.sh'
    logfile="$APPVM_SHOTS_DIR/imgurl.log"
    echo "$UPLOADHELPER" | qvm-run --pass-io $appvm "cat > $APPVM_SHOTS_DIR/$uploadername"
@@ -205,6 +275,7 @@ if [ X"$appvm" != X"" ]; then
    qvm-run $appvm "zenity --text-info --width=500 --height=180 --modal --filename=$logfile --text Ready" 
    echo $RESULT
 
+   #done
 else
    echo "[-] no AppVM name provided"
 fi
